feb893e16cebcfdeff1e56dfc6ddb65ad8f3bed4
Masamichi Kawamura <mk1236ec06r1@gmail.com>
2020-10-29 12:10:36 +0900

    プロジェクト ファイルを追加します。

---
+++ Form1.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ファイルToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.新規作成ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.開くToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.保存ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.閉じるToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.MenuStrip1.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ファイルToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ファイルToolStripMenuItem
+        '
+        Me.ファイルToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.新規作成ToolStripMenuItem, Me.開くToolStripMenuItem, Me.保存ToolStripMenuItem, Me.閉じるToolStripMenuItem})
+        Me.ファイルToolStripMenuItem.Name = "ファイルToolStripMenuItem"
+        Me.ファイルToolStripMenuItem.Size = New System.Drawing.Size(53, 20)
+        Me.ファイルToolStripMenuItem.Text = "ファイル"
+        '
+        '新規作成ToolStripMenuItem
+        '
+        Me.新規作成ToolStripMenuItem.Name = "新規作成ToolStripMenuItem"
+        Me.新規作成ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.新規作成ToolStripMenuItem.Text = "新規作成"
+        '
+        '開くToolStripMenuItem
+        '
+        Me.開くToolStripMenuItem.Name = "開くToolStripMenuItem"
+        Me.開くToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.開くToolStripMenuItem.Text = "開く"
+        '
+        '保存ToolStripMenuItem
+        '
+        Me.保存ToolStripMenuItem.Name = "保存ToolStripMenuItem"
+        Me.保存ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.保存ToolStripMenuItem.Text = "保存"
+        '
+        '閉じるToolStripMenuItem
+        '
+        Me.閉じるToolStripMenuItem.Name = "閉じるToolStripMenuItem"
+        Me.閉じるToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.閉じるToolStripMenuItem.Text = "閉じる"
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 428)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(800, 22)
+        Me.StatusStrip1.TabIndex = 1
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(43, 17)
+        Me.ToolStripStatusLabel1.Text = "行数："
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TabControl1.Location = New System.Drawing.Point(0, 24)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(800, 404)
+        Me.TabControl1.TabIndex = 2
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.DefaultExt = "txt"
+        Me.OpenFileDialog1.Filter = "txt file(*.txt)|*.txt"
+        Me.OpenFileDialog1.Title = "ファイルオープン"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "簡易テキストエディタ"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ファイルToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents 新規作成ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 開くToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 保存ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 閉じるToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+End Class
---
+++ Form1.vb
@@ -0,0 +1,109 @@
+﻿Imports System.IO
+Imports System.Reflection.Emit
+
+Public Class Form1
+
+    Dim tab As TabPage
+    WithEvents tb As TextBox
+    Dim fn As Font = New Font("MS Gothic", 12)
+
+    Private Sub Form1_Load() Handles MyBase.Load
+        tb = New TextBox
+        With tb
+            .ScrollBars = ScrollBars.Both
+            .Multiline = True
+            .Font = fn
+            .Size = New Size(TabControl1.Width, TabControl1.Height)
+            .Dock = DockStyle.Fill
+        End With
+        tab = New TabPage
+        tab.Text = IO.Path.GetFileName("新規")
+        tab.Tag = "新規"
+        TabControl1.TabPages.Add(tab)
+        tab.Controls.Add(tb)
+        lineDisp()
+    End Sub
+    Private Sub lineDisp() Handles tb.Click
+        '行数表示
+        TabControl1.Tag = CType(TabControl1.SelectedTab.Controls(0), TextBox).GetLineFromCharIndex(tb.SelectionStart) + 1
+        ToolStripStatusLabel1.Text = TabControl1.Tag.ToString + "行"
+        Me.Focus() 'textboxをクリックすると選択解除されるため
+    End Sub
+
+    Public Sub tabcontrol_Keydown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles tb.KeyDown
+        If e.KeyData = Keys.Down Or Keys.Up Then
+            Call lineDisp()
+        End If
+    End Sub
+
+    Private Sub 開くToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles 開くToolStripMenuItem.Click
+        OpenFileDialog1.InitialDirectory = My.Settings.file
+        OpenFileDialog1.ShowDialog()
+        If My.Settings.fname <> "" Then
+            fn = New Font(My.Settings.fname, 12)
+        End If
+    End Sub
+
+    Private Sub openFileDialog_OK() Handles OpenFileDialog1.FileOk
+
+        tb = New TextBox
+        With tb
+            .ScrollBars = ScrollBars.Both
+            .Multiline = True
+            .Font = fn
+            .Size = New Size(TabControl1.Width, TabControl1.Height)
+            .Dock = DockStyle.Fill
+        End With
+        tab = New TabPage
+        tab.Text = IO.Path.GetFileName(OpenFileDialog1.FileName)
+        tab.Tag = OpenFileDialog1.FileName
+        tb.Text = File.ReadAllText(OpenFileDialog1.FileName, System.Text.Encoding.GetEncoding("SHIFT-jis"))
+        My.Settings.file = Path.GetDirectoryName(OpenFileDialog1.FileName)
+        TabControl1.TabPages.Add(tab)
+        tab.Controls.Add(tb)
+        TabControl1.SelectedIndex = TabControl1.TabCount + 1
+        lineDisp()
+
+    End Sub
+
+    Private Sub 新規作成ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles 新規作成ToolStripMenuItem.Click
+        'タブ追加
+        If TabControl1.TabCount <= 5 Then
+            tb = New TextBox
+            With tb
+                .ScrollBars = ScrollBars.Both
+                .Multiline = True
+                .Font = fn
+                .Size = New Size(TabControl1.Width, TabControl1.Height)
+                .Dock = DockStyle.Fill
+            End With
+            tab = New TabPage
+            tab.Text = IO.Path.GetFileName("新規")
+            tab.Tag = "新規"
+            TabControl1.TabPages.Add(tab)
+            tab.Controls.Add(tb)
+            lineDisp()
+        Else
+            MsgBox("一度に編集できるファイルの数は5つまでです!", 16)
+        End If
+    End Sub
+
+    Private Sub 保存ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles 保存ToolStripMenuItem.Click
+        If MsgBox("変更を保存しますか？", 36) = vbYes Then
+            Dim fileName As String = TabControl1.SelectedTab.Tag
+            If "新規".Equals(TabControl1.SelectedTab.Tag) Then
+                fileName = fileName + ".txt"
+            End If
+            File.WriteAllText(fileName, CType(TabControl1.SelectedTab.Controls(0), TextBox).Text,
+                              System.Text.Encoding.GetEncoding("SHIFT-jis"))
+        End If
+        TabControl1.SelectedTab.Dispose()
+        If TabControl1.TabCount = 0 Then
+            Application.Exit()
+        End If
+    End Sub
+
+    Private Sub 閉じるToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles 閉じるToolStripMenuItem.Click
+        Application.Exit()
+    End Sub
+End Class
